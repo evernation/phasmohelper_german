c104df2a7922a962647d0907ef307a050a8700d4
Johan <johan.tylon@gmail.com>
2023-03-21 00:51:36 +0100

    Add 'R' hotkey to reset

--- main.js
+++ main.js
@@ -349,3 +349,9 @@ $(".evidence>li").each(function() {
 	else if (evidence.textContent === 'EMF Level 5')
 		evidence.textContent = 'EMF 5';
 });
+
+$(document).on('keydown', function(e) {
+	if (e.key === 'r') {
+		reset();
+	}
+});
